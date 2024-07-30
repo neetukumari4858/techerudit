f7376030fc8001e3b89661a1cb9eeb800f9b8e9a
neetukumari4858 <neetu21@navgurukul.org>
2024-07-30 21:08:41 +0530

    minor changes

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,9 @@
 const Contact = () => {
     return (
-        <h3>Contact Us</h3>
-        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua</p>
+        <div>
+            <h3>Contact Us</h3>
+            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua</p>
+        </div>
     )
 }
 export default Contact;
